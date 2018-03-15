535ce68ae392a464cedeeb5620058dfcd85f4311
Trevor Seitz <trevinla@gmail.com>
2018-03-15 17:33:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
   words = []
   textarray = text.split(" ")
   textarray.each do |word|
-    if word.match(/^un/) && word.match(/$ing/)
+    if word.match(/^un/) || word.match(/$ing/)
       words << word
     end
   end
