488ac2d943ffebddb6df9ee62d80a3fa36cb3753
Trevor Seitz <trevinla@gmail.com>
2018-03-15 17:00:08 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  words = []
+  if word.match(/^un/) || word.match(/$ing/)
+    words << text
+  end
+  words
 end
 
 def words_five_letters_long(text)
