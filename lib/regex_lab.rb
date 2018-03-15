f7d9dc8634b02c0cb19c07b08f3663c4f36fdadf
Trevor Seitz <trevinla@gmail.com>
2018-03-15 17:57:42 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -33,7 +33,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\A[A-Z]/) != nil
     # binding.pry
-    if text.match(/\$[:punct:]/) != nil
+    if text.match(/\$[:punct:]/) 
     # binding.pry
       true
     else
