2e77af43bad5a3fbbda4e344169623a006e3dd01
Trevor Seitz <trevinla@gmail.com>
2018-03-15 17:23:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
   words = []
   textarray = text.split(" ")
   textarray.each do |word|
-    if word.match(/^un/) || word.match(/$ing/)
+    if word.match(/^un/) && word.match(/$ing/)
       words << word
     end
   end
@@ -31,7 +31,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z]/) && text.match(/$[.?!]/)
 end
 
 def valid_phone_number?(phone)
