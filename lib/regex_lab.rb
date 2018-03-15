d8fb19a719408c3678e92bb51828dfcb9be78653
Trevor Seitz <trevinla@gmail.com>
2018-03-15 18:56:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -35,7 +35,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.size == 10 && phone.match(/[0-9]/)
+  phone.size == 10 && phone.include?(/[0-9]/)
   binding.pry
 
   # puts phone.size
