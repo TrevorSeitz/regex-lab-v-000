c51e3ea72174a3078762052920e6ed820f1fdab8
Trevor Seitz <trevinla@gmail.com>
2018-03-15 17:41:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -31,8 +31,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z]/) || text.match(/$[.?!]/)
-    true
+  if text.match(/^[A-Z]/) 
+    if text.match(/$[.?!]/)
+      true
+    end
   else
     false
   end
