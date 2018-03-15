f9231032bab4c396c4a356622ff4113fb7ca26fd
Trevor Seitz <trevinla@gmail.com>
2018-03-15 17:59:16 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -31,7 +31,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]/) != nil && text.match(/\$[:punct:]/)
+  if text.match(/\A[A-Z]/) != nil && if text.match(/\$[:punct:]/)
     # binding.pry
     # if text.match(/\$[:punct:]/)
     # binding.pry
